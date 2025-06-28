f3e1ae5a44671701d66d8d6b9adf67173f26f1da
Ryan Rumsey <69648919+rrumsey95@users.noreply.github.com>
2025-06-28 12:09:27 -0600

    added sg to ec2 block

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "audio-curl-bot-ec2" {
   ami = var.ec2_ami
   instance_type = "t2.micro"
   user_data = file("audio-curl-bot-ec2-user-data.sh")
-  // subnet_id = "subnet-0065cc1c08538f592"
+  subnet_id = aws_subnet.audio-curl-bot-subnet.id
+  vpc_security_group_ids = [aws_security_group.bot_networking.id, aws_security_group.bot_ssh_networking.id]
 
   tags = {
     Name = "audio-curl-bot-ec2"
