3d52e3469f82456eb5137ac3487fd54ebe8bcd5e
Ryan Rumsey <69648919+rrumsey95@users.noreply.github.com>
2025-06-28 12:57:41 -0600

    fixed subnet id

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "audio-curl-bot-ec2" {
   ami = var.ec2_ami
   instance_type = "t2.micro"
   user_data = file("audio-curl-bot-ec2-user-data.sh")
-  subnet_id = aws_subnet.audio-curl-bot-subnet.id
+  subnet_id = aws_subnet.public_subnets[0].id
   vpc_security_group_ids = [aws_security_group.bot_networking.id, aws_security_group.bot_ssh_networking.id]
   key_name = var.ec2_key_name_putty
 
