695a6b29233b26e0c00cd8a8c2ea955b2adcdc94
Ryan Rumsey <69648919+rrumsey95@users.noreply.github.com>
2025-06-28 21:51:18 -0600

    updated the user data script update

--- IaC/ec2.tf
+++ IaC/ec2.tf
@@ -5,6 +5,9 @@ resource "aws_instance" "audio-curl-bot-ec2" {
   subnet_id = aws_subnet.public_subnets[0].id
   vpc_security_group_ids = [aws_security_group.bot_networking.id, aws_security_group.bot_ssh_networking.id]
   key_name = var.ec2_key_name_putty
+  user_data_replace_on_change = true
+  // associate_public_ip_address = true
+  // iam_instance_profile = aws_iam_instance_profile.audio-curl-bot-ec2-profile.name
 
   tags = {
     Name = "audio-curl-bot-ec2"
